f855678bfb6136c490494397356aed1963ed3bb0
Zamo797 <nomzamonhlap97@gmail.com>
2026-04-15 15:27:54 +0200

    adding database schemas

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -31,8 +30,5 @@ app.get("/api/db-check", async (req, res) => {
     });
   }
 });
-
-app.use(notFound);
-app.use(errorHandler);
 
 export default app;
