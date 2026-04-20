b1b0ba536c454ef1d06afb8b1eb1c27f0ac94b59
Comfort Ngwenya <nqobilecomfyngwenya@gmail.com>
2026-04-20 09:52:40 +0200

    feat(transactions): add controllers for sendCash, transaction history, airtime, and electricity

---
+++ Backend/src/controllers/transactionController.js
@@ -0,0 +1,69 @@
+import * as transactionService from "../services/transactionService.js";
+
+export const sendCash = async (req, res, next) => {
+  try {
+    const { accountId } = req.params;
+    const transaction = await transactionService.sendCash(req.user._id, accountId, req.body);
+    res.status(201).json(transaction);
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ 
+        success: false,
+        message: err.message || "Failed to sendCash",
+    });
+    next(err);
+  }
+};
+
+export const getTransactionHistory = async (req, res, next) => {
+  try {
+    const { accountId } = req.params;
+    const result = await transactionService.getTransactionHistory(
+      accountId,
+      req.user._id,
+      req.query
+    );
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ 
+        success: false,
+        message: err.message || "Failed to transaction history",
+    });
+    next(err);
+  }
+};
+
+export const purchaseAirtime = async (req, res, next) => {
+  try {
+    const { accountId } = req.params;
+    const transaction = await transactionService.purchaseAirtime(
+      req.user._id,
+      accountId,
+      req.body
+    );
+    res.status(201).json(transaction);
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ 
+        success: false,
+        message: err.message || "Failed to purchase airtime",
+    });
+    next(err);
+  }
+};
+
+export const purchaseElectricity = async (req, res, next) => {
+  try {
+    const { accountId } = req.params;
+    const transaction = await transactionService.purchaseElectricity(
+      req.user._id,
+      accountId,
+      req.body
+    );
+    res.status(201).json(transaction);
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ 
+        success: false,
+        message: err.message || "Failed to purchase electricity",
+    });
+    next(err);
+  }
+};
